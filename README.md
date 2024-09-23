e1addb3b54c26446c1a90b0e94857a6946a235f2
ssailer1 <ssailer@student.tgm.ac.at>
2024-09-23 09:24:35 +0200

    Initial commit, UML und Gradle setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Worttrainer reloaded
+@auhtor: Sebastian Sailer
+@version: 20.09.2024
+===
+
+
---
+++ app/src/main/java/control/WortTrainerControl.java
@@ -0,0 +1,6 @@
+package control;
+public class WortTrainerControl {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
